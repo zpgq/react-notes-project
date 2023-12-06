2292c2841f7a6d1550adbdf997bac76129c2b747
zpg <327774162@qq.com>
2023-12-06 22:35:16 +0800

    fix: 公共样式目录调整

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // import React from 'react';
 import ReactDOM from "react-dom";
 import App from "./app";
-import "./index.css";
-import "./global.less";
+import "./styles/index.css";
+import "./styles/global.less";
 
 ReactDOM.render(<App />, document.getElementById("app"));
--- src/modules/test-page/index.jsx
+++ src/modules/test-page/index.jsx
@@ -1,7 +1,7 @@
 const Test = () => {
   return (
     <div>
-      <h1>Test</h1>
+      <h1>regengx</h1>
     </div>
   )
 }
